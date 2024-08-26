3d8d33942343e5d5a00d440f94f48a9830580254
everydayspring <lee.bom.952@gmail.com>
2024-08-26 11:31:53 +0900

    create project

---
+++ src/main/java/com/sparta/springjpascheduler/SpringJpaSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.springjpascheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaSchedulerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJpaSchedulerApplication.class, args);
+    }
+
+}

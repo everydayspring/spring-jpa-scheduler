be6eeb4dac4fa2eaa1f8df73d9399c0cd2b763d1
everydayspring <lee.bom.952@gmail.com>
2024-08-30 13:58:22 +0900

    requestDto @NoArgsConstructor 추가

--- src/main/java/com/sparta/springjpascheduler/dto/CommentRequestDto.java
+++ src/main/java/com/sparta/springjpascheduler/dto/CommentRequestDto.java
@@ -1,8 +1,10 @@
 package com.sparta.springjpascheduler.dto;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class CommentRequestDto {
     private Long userId;
     private String content;
--- src/main/java/com/sparta/springjpascheduler/dto/ManagerRequestDto.java
+++ src/main/java/com/sparta/springjpascheduler/dto/ManagerRequestDto.java
@@ -1,8 +1,10 @@
 package com.sparta.springjpascheduler.dto;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class ManagerRequestDto {
     private Long todoId;
     private Long todoUserId;
